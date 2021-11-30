f8c8ca9e56f09dd166c8448fa92850783611db4c
jay <jaifegde2@gmail.com>
2021-11-30 14:49:14 +0530

    initial commit

---
+++ .net core/Solution1/Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Calfunctions;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int secondNum;
+            Console.WriteLine("Enter the First number:");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second number:");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+
+            Function obj = new Function();
+ 
+            Console.WriteLine("Your Addition is:"+ obj.Sum(firstNum,secondNum));
+
+            Console.WriteLine("Your Substraction is:" + obj.Substraction(firstNum, secondNum));
+
+            Console.WriteLine("Your Multiplication is:" + obj.Multiplication(firstNum, secondNum));
+
+            Console.WriteLine("Your Division is:" + obj.Division(firstNum, secondNum));
+
+
+            Console.ReadLine();
+            
+            
+
+            
+        }
+    }
+}
---
+++ .net core/Solution1/Calfunctions/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Calfunctions
+{
+    public class Function
+    {
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+
+
+
+
+    }
+
+}

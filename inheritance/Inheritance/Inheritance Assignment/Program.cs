a3180ebea604b4b205d8e5035e6a46b2c0dc4aa9
jay <jaifegde2@gmail.com>
2021-12-01 17:00:02 +0530

    OOPS assignment

---
+++ inheritance/Inheritance/Inheritance Assignment/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace Inheritance_Assignment
+{
+    public class Phone
+    {
+        public string model;
+        public int prize;
+        public string processor;
+        public string ram;
+        private string os;
+
+
+
+        public void PhoneInfo()
+        {
+            Console.WriteLine("model :" + model);
+            Console.WriteLine("Prize :" + prize);
+            Console.WriteLine("Processor :" + processor);
+            Console.WriteLine("Ram :" + ram);
+            Console.WriteLine("Os :" + os);
+        }
+        public string OperatingSystem()
+        {
+            os = "ios";
+            return os;
+        }
+
+}
+    class Iphone : Phone
+    {
+        string modelSize = "20x8cm";
+        public void PrintSize()
+        {
+            Console.WriteLine("Model Size is :"+ modelSize);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Iphone ios = new Iphone
+            {
+                model = "13 Pro max",
+                processor = "A13",
+                ram = "6gb"
+            };
+            ios.OperatingSystem();
+            ios.prize = 120000;
+            ios.PhoneInfo();
+            ios.PrintSize();
+        }
+    }
+}
